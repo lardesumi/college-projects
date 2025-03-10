a4bf5cd9585c70bde4eaf5bf4ac103c0b10e7501
lardesumi <146085082+lardesumi@users.noreply.github.com>
2025-03-10 18:37:29 -0300

    Create pythonton.py

---
+++ pythonton.py
@@ -0,0 +1,9 @@
+# Receber dois npumeros do usuário
+numero1 = float (input("Digite o primeiro numero: "))
+numero2 = float (input("Digite o segundo numero: ")) 
+
+# Calcular a média
+media = (numero1 + numero2) /2
+
+# Imprimir a média
+print (f"A média dos dois números é: {media}")
